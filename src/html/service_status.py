21c8974ea9ad015f2981a4218a94d7b8a1346465
Rob <robe16@hotmail.co.uk>
2017-11-18 23:38:54 +0000

    Service status page created to display services discovered and offline/online status

--- src/html/service_status.py
+++ src/html/service_status.py
@@ -19,10 +19,15 @@ def create_servicestatus(services):
         #
         args = {'service_name': services[s]['name'],
                 'status': status,
-                'img': '/img/logo/{img}'.format(img=img)}
+                'groups': services[s]['groups'],
+                'img': '/img/services/{img}'.format(img=img)}
         #
         page_body += urlopen('resources/html/service_status/service.html').read().encode('utf-8').format(**args)
     #
+    if page_body == '':
+        page_body = urlopen('resources/html/service_status/service_null.html').read().encode('utf-8')
+    #
     return create_page(services,
                        page_body,
-                       title='Jarvis')
+                       title='Jarvis',
+                       header='Service status')
