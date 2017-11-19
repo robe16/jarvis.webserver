9f878e294a9fe90cfd814e8422e27be256a89ae7
Rob <robe16@hotmail.co.uk>
2017-11-19 10:20:50 +0000

    Addition of lhs and rhs menu bars with home and servicestatus links.

--- src/html/menu.py
+++ src/html/menu.py
@@ -1,5 +1,16 @@
 from urllib import urlopen
+from resources.global_resources.variables import uri_servicestatus
 
 
 def html_menu(services):
-    return ''
+    html = _html_menu_lhs()
+    html += _html_menu_rhs()
+    return html
+
+
+def _html_menu_lhs():
+    return urlopen('resources/html/common/menu_lhs.html').read().encode('utf-8').format(menu='')
+
+
+def _html_menu_rhs():
+    return urlopen('resources/html/common/menu_rhs.html').read().encode('utf-8').format(uri_servicestatus=uri_servicestatus)
