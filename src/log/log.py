f88658aea2dfc43560109710330f9e87ed9f613d
Rob <robe16@hotmail.co.uk>
2017-12-09 21:21:28 +0000

    Bug fix - conflicting module names causing error on imports.

--- src/log/log.py
+++ src/log/log.py
@@ -3,11 +3,11 @@ import logging
 import os
 
 from resources.global_resources.variables import serviceType, serviceId
-from resources.global_resources.log import logTimeformat
-from resources.global_resources.log import logPass, logFail, logException
-from resources.global_resources.log import logMsg_Inbound_Info, logMsg_Inbound_Error
-from resources.global_resources.log import logMsg_Internal_Info, logMsg_Internal_Error
-from resources.global_resources.log import logMsg_Outbound_Info, logMsg_Outbound_Error
+from resources.global_resources.log_vars import logTimeformat
+from resources.global_resources.log_vars import logPass, logFail, logException
+from resources.global_resources.log_vars import logMsg_Inbound_Info, logMsg_Inbound_Error
+from resources.global_resources.log_vars import logMsg_Internal_Info, logMsg_Internal_Error
+from resources.global_resources.log_vars import logMsg_Outbound_Info, logMsg_Outbound_Error
 
 
 def log_inbound(result, client, uri, method, httpresponse, desc='-', exception=False):
