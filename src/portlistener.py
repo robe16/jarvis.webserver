2999d678010e9bc3e44b0eec7ec476d33d592fc5
Rob <robe16@hotmail.co.uk>
2017-12-10 22:54:47 +0000

    Correction to bottleDict_to_string code to include " symbol

--- src/portlistener.py
+++ src/portlistener.py
@@ -223,7 +223,7 @@ def _convert_query_to_string(bottleDict):
     str = '{'
     for k in bottleDict:
         str += ', ' if not str == '{' else ''
-        str += '{key}:{value}'.format(key=k, value=bottleDict[k])
+        str += '"{key}":"{value}"'.format(key=k, value=bottleDict[k])
     str += '}'
     #
     return str
