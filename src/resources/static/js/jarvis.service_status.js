0c8f3bc265140c8fc88e2d321f0962f98e6eb0ee
Rob <robe16@hotmail.co.uk>
2017-12-11 19:57:15 +0000

    Change to JS for sending HTTP requests to webserver app - to test and tweak in coming commits.

---
+++ src/resources/static/js/jarvis.service_status.js
@@ -0,0 +1,25 @@
+function removeService(serviceID) {
+    try {
+        // Confirmation box
+        r = confirm("You are about to remove service '" + serviceID + '\n\nThe service will only be added again to the list of available services once re-discovered by the web server.\n\nAre you sure?");
+        //
+        if (r == true){
+            //
+            var url = '/services/remove/' + serviceID
+            //
+            success = deleteHttp(url, false)
+            //
+            if (success == true) {
+                alert("Service " + serviceID + "has been removed and will only be added to list of available services once re-discovered.")
+            }
+            //
+        } else {
+            //
+            alert("Action cancelled\n\nService " + serviceID + "has not been removed.")
+            //
+        }
+    }
+    catch(err) {
+        alert("An error has been encountered, please try again.\n\n" + err)
+    }
+}
--- src/resources/static/js/send_http.js
+++ src/resources/static/js/send_http.js
@@ -1,4 +1,95 @@
+function getHttp(url, alert) {
+    //
+    try {
+        var xmlHttp = commonHttp(url=url, method="GET");
+//        var xmlHttp = new XMLHttpRequest();
+//        xmlHttp.open("GET", url, false);
+//        xmlHttp.send(data);
+        //
+        if (alert) {alertTrigger(xmlHttp.status==200)}
+        if (xmlHttp.status==200) {return xmlHttp.responseText;} else {return false;}
+        //
+    }
+    catch(err) {
+        alertTrigger(false);
+        return false;
+    }
+}
+
+function postHttp(url, alert, payload) {
+    //
+    try {
+        var xmlHttp = commonHttp(url=url, method="POST", payload=payload);
+        //
+        if (alert) {alertTrigger(xmlHttp.status==200)}
+        if (xmlHttp.status==200) {return true} else {return false;}
+        //
+    }
+    catch(err) {
+        alertTrigger(false);
+        return false;
+    }
+}
+
+function deleteHttp(url, alert) {
+    //
+    try {
+        var xmlHttp = commonHttp(url=url, method="DELETE");
+        //
+        if (alert) {alertTrigger(xmlHttp.status==200)}
+        if (xmlHttp.status==200) {return true} else {return false;}
+        //
+    }
+    catch(err) {
+        alertTrigger(false);
+        return false;
+    }
+}
+
+
+function alertTrigger(success) {
+    if (success) {
+        var v = document.getElementById('cmd_success')
+    } else {
+        var v = document.getElementById('cmd_fail')
+    }
+    v.style.display="block";
+    setTimeout(function(){v.style.display="none"}, 2000);
+}
+
+
+function commonHttp(url, method, payload) {
+    //
+    payload = payload || null;
+    //
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url, true); // true = async
+    //
+    xhr.onload = function (e) {
+      if (xhr.readyState === 4) {
+        return xhr;
+      }
+    };
+    xhr.onerror = function (e) {
+      console.error(xhr.statusText);
+      return False
+    };
+    //
+    xhr.send(payload);
+}
+
+
+
+
+
 function sendHttp(url, data, method, responserequired, alert) {
+    //
+    url = url || "/";
+    data = data || "/";
+    method = method || "GET"; // Assume 'get' if not defined
+    responserequired = responserequired || 0
+    alert = alert || false
+    //
     // responserequired:
     // 0 = none
     // 1 = text/body of response
