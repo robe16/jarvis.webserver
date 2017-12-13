b89d5eb8603b65caee4285ab8b8617cf02527fb1
Rob <robe16@hotmail.co.uk>
2017-12-13 22:05:01 +0000

    Development of group page to include all services within the group

--- src/html/menu.py
+++ src/html/menu.py
@@ -3,7 +3,7 @@ from common_functions.urlencode import url_encode
 from resources.global_resources.variables import uri_servicestatus
 from resources.global_resources.services import service_variables
 from discovery.group_services import group_services
-from resources.groups.groups_functions import get_group_category_list, get_group_image
+from resources.groups.groups_functions import get_group_image
 
 
 def html_menu(services):
@@ -20,61 +20,6 @@ def _html_menu_lhs(services):
     ################
     #
     grouped_services = group_services(services)
-    #
-    # for c in grouped_services.keys():
-    #     #
-    #     for g in grouped_services[c].keys():
-    #         #
-    #         try:
-    #             #
-    #             html_group = ''
-    #             #
-    #             for service in grouped_services[c][g]['services']:
-    #                 #
-    #                 type = services[service]['service_type']
-    #                 if type in service_variables:
-    #                     img = service_variables[type]['type']
-    #                 else:
-    #                     img = 'logo_other.png'
-    #                 #
-    #                 args = {'id': service,
-    #                         'class': 'menu_item_service',
-    #                         'href': '/service/page/{service_id}'.format(service_id=service),
-    #                         'name': services[service]['name_long'],
-    #                         'img': '/img/service/{img}'.format(img=img)}
-    #                 #
-    #                 html_group += urlopen('resources/html/common/menu_item.html').read().encode('utf-8').format(**args)
-    #             #
-    #             for subservice in grouped_services[c][g]['subservices']:
-    #                 #
-    #                 type = services[subservice['service_id']]['service_type']
-    #                 if type in service_variables:
-    #                     img = service_variables[type]['type']
-    #                 else:
-    #                     img = 'logo_other.png'
-    #                 #
-    #                 args = {'id': subservice['service_id'],
-    #                         'class': 'menu_item_service',
-    #                         'href': '/service/page/{service_id}?group={group}'.format(service_id=subservice['service_id'],
-    #                                                                                   group=url_encode(g)),
-    #                         'name': services[subservice['service_id']]['name_long'],
-    #                         'img': '/img/service/{img}'.format(img=img)}
-    #                 #
-    #                 html_group += urlopen('resources/html/common/menu_item.html').read().encode('utf-8').format(**args)
-    #             #
-    #             img = get_group_image(g)
-    #             img = img if img else 'logo_other.png'
-    #             #
-    #             args = {'group_id': '{info}'.format(info=url_encode(g)),
-    #                     'group_name': g,
-    #                     'group_img': '/img/group/{img}'.format(img=img),
-    #                     'html_group_items': html_group}
-    #             #
-    #             html += urlopen('resources/html/common/menu_item_group.html').read().encode('utf-8').format(**args)
-    #             #
-    #             #
-    #         except:
-    #             pass
     #
     for c in grouped_services.keys():
         #
--- src/service_page/groups.py
+++ src/service_page/groups.py
@@ -1,4 +1,66 @@
-
+from urllib import urlopen
+from common_functions.urlencode import url_encode, url_decode
+from resources.global_resources.services import service_variables
+from resources.groups.groups_functions import get_group_category, get_group_image
+from discovery.group_services import group_services
+from services import serviceHtml
 
 def groupPage(services, group_id):
+    #
+    grouped_services = group_services(services)
+    #
+    group_name = url_decode(group_id)
+    category = get_group_category(group_name)
+    #
+    try:
+        #
+        html_buttons = ''
+        html_pages = ''
+        #
+        for service in grouped_services[category][group_name]['services']:
+            #
+            type = services[service]['service_type']
+            if type in service_variables:
+                img = service_variables[type]['type']
+            else:
+                img = 'logo_other.png'
+            #
+            args = {'href': '#',
+                    'img': img,
+                    'name': services[service]['name_long']}
+            #
+            html_buttons += urlopen('resources/html/services/group_service_img.html').read().encode('utf-8').format(**args)
+            #
+            html_pages += serviceHtml(services, services[service]['service_id'])
+            #
+        #
+        for subservice in grouped_services[category][group_name]['subservices']:
+            #
+            # TODO
+            pass
+            #
+            # type = services[subservice['service_id']]['service_type']
+            # if type in service_variables:
+            #     img = service_variables[type]['type']
+            # else:
+            #     img = 'logo_other.png'
+            # #
+            # args = {'id': subservice['service_id'],
+            #         'class': 'menu_item_service',
+            #         'href': '/service/page/{service_id}?group={group}'.format(service_id=subservice['service_id'],
+            #                                                                   group=url_encode(g)),
+            #         'name': services[subservice['service_id']]['name_long'],
+            #         'img': '/img/service/{img}'.format(img=img)}
+            # #
+            # html_group += urlopen('resources/html/common/menu_item.html').read().encode('utf-8').format(**args)
+        #
+        args = {'service_buttons': html_buttons,
+                'service_pages': html_pages}
+        #
+        return urlopen('resources/html/services/group.html').read().encode('utf-8').format(**args)
+        #
+        #
+    except:
+        pass
+    #
     return ''
