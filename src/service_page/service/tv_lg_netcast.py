dc19f2eea2cbeb4287eaa9215e54e3672a6a402a
Rob <robe16@hotmail.co.uk>
2017-12-15 00:15:05 +0000

    Extract app icon code from hardcoded into python and as html resource.

--- src/service_page/service/tv_lg_netcast.py
+++ src/service_page/service/tv_lg_netcast.py
@@ -30,12 +30,12 @@ def _html_apps(service):
                 if count % 4 == 0:
                     html += '</tr><tr style="height:35px; padding-bottom:2px; padding-top:2px">'
                 #
-                html += ('<td class="grid_item" style="width: 20%; cursor: pointer; vertical-align: top;" align="center" onclick="sendHttp(\'/service/command/{service_id}?command=executeApp&auid={auid}\', null, \'POST\', false, true)">' +
-                         '<img src="/service/image/{service_id}/appIcon?auid={auid}" style="height:50px;"/>' +
-                         '<p style="text-align:center; font-size: 13px;">{name}</p>' +
-                         '</td>').format(service_id=service['service_id'],
-                                         auid=json_applist[app]['auid'],
-                                         name=json_applist[app]['name'])
+                args = {'service_id': service['service_id'],
+                        'auid': json_applist[app]['auid'],
+                        'name': json_applist[app]['name']}
+                #
+                html += urlopen('resources/html/services/tv_lg_netcast/app_button.html').read().encode('utf-8').format(**args)
+                #
                 count += 1
                 #
             except Exception as e:
