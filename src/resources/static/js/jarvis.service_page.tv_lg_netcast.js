1d110b7fe8c1fcda830e2df98a3ac8392242dd77
Rob <robe16@hotmail.co.uk>
2017-12-31 22:21:54 +0000

    Further development and updates to tv_lg_netcast trackpad (to test)

--- src/resources/static/js/jarvis.service_page.tv_lg_netcast.js
+++ src/resources/static/js/jarvis.service_page.tv_lg_netcast.js
@@ -3,50 +3,44 @@ function tvlgnetcast_updateScreenshot(service_id) {
     document.getElementById(service_id + "_screenshot").src = newSrc;
 }
 
-var tvlgnetcast_touch_flag = {};
 
-function tvlgnetcast_touch_mouseDown(service_id) {
+function tvlgnetcast_touchpad(service_id) {
     //
-    tvlgnetcast_touch_flag[service_id] = true;
+    var trackpad_id = "trackpad-" + service_id;
+    var trackpad_obj = document.getElementById(trackpad_id);
     //
     var last_x = 0;
     var last_y = 0;
-    var current_x = 0;
-    var current_y = 0;
-    var deltaX = 0;
-    var deltaY = 0;
     //
-    try {
+    // http://www.javascriptkit.com/javatutors/touchevents.shtml
+    //
+    trackpad_obj.addEventListener('touchstart', function(e) {
         //
-        while (tvlgnetcast_touch_flag[service_id]) {
-            //
-            var trackpad_id = "trackpad-" + service_id;
-            // var trackpad_obj = document.getElementById(trackpad_id);
-            //
-            var offset = $("#" + trackpad_id).offset();
-            $(document).mousemove(function(e){
-                current_x = e.pageX - offset.left;
-                current_y = e.pageY - offset.top;
-            });
-            //
-            deltaX = last_x - current_x;
-            deltaY = last_y - current_y;
-            //
-            sendCommand(service_id, {command: 'touchMove', touchMoveX: deltaX, touchMoveY: deltaY});
-            //
-            last_x = current_x;
-            last_y = current_y;
-            //
-            wait(500); //0.5sec
-            //
-        }
-    }
-    catch(err) {
-        alert("An error has been encountered, please try again.\n\n" + err)
-    }
-}
-
-
-function tvlgnetcast_touch_mouseUp(service_id) {
-    tvlgnetcast_touch_flag[service_id] = false;
+        last_x = parseInt(trackpad_obj.clientX);
+        last_y = parseInt(trackpad_obj.clientY);
+        //
+    }, false)
+    //
+    trackpad_obj.addEventListener('touchmove', function(e) {
+        //
+        var current_x = parseInt(trackpad_obj.clientX);
+        var current_y = parseInt(trackpad_obj.clientY);
+        //
+        var deltaX = last_x - current_x;
+        var deltaY = last_y - current_y;
+        //
+        sendCommand(service_id, {command: 'touchMove', touchMoveX: deltaX, touchMoveY: deltaY});
+        //
+        last_x = current_x;
+        last_y = current_y;
+        //
+        wait(500); //0.5sec
+        //
+    }, false)
+    //
+    trackpad_obj.addEventListener('touchend', function(e) {
+        //
+    }, false)
+    //
+    //
 }
