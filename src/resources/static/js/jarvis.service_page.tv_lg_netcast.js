5d4cdd2e3ddd1b4b0fc2f59d15cb51fa827319f3
Rob <robe16@hotmail.co.uk>
2017-12-31 18:57:28 +0000

    Further development and updates to tv_lg_netcast trackpad (to test)

--- src/resources/static/js/jarvis.service_page.tv_lg_netcast.js
+++ src/resources/static/js/jarvis.service_page.tv_lg_netcast.js
@@ -33,3 +33,35 @@ function tvlgnetcast_touch_mouseMove(service_id) {
         alert("An error has been encountered, please try again.\n\n" + err)
     }
 }
+
+
+
+function tvlgnetcast_touchMove(service_id) {
+    //
+    var last_x
+    var last_y
+    //
+    try {
+        //
+        while(MouseEvent.which==1) {
+            //
+            trackpad_id = "trackpad-" + service_id;
+            var trackpad_obj = document.getElementById(trackpad_id);
+            //
+            current_x = instanceOfMouseEvent.clientX;
+            current_y = instanceOfMouseEvent.clientY;
+            //
+            deltaX = last_x - current_x;
+            deltaY = last_y - current_y;
+            //
+            sendCommand(service_id, {command: 'touchMove', touchMoveX: touchX, touchMoveY: touchY});
+            //
+            last_x = current_x;
+            last_y = current_y;
+            //
+        }
+    }
+    catch(err) {
+        alert("An error has been encountered, please try again.\n\n" + err)
+    }
+}
