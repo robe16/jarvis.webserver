7c02e93ab838a2f3867b0e33cc7e5abce692fbba
Rob <robe16@hotmail.co.uk>
2017-12-31 19:41:49 +0000

    Further development and updates to tv_lg_netcast trackpad (to test)

--- src/resources/static/js/jarvis.service_page.tv_lg_netcast.js
+++ src/resources/static/js/jarvis.service_page.tv_lg_netcast.js
@@ -3,26 +3,36 @@ function tvlgnetcast_updateScreenshot(service_id) {
     document.getElementById(service_id + "_screenshot").src = newSrc;
 }
 
+var tvlgnetcast_touch_flag = {};
 
 function tvlgnetcast_touch_mouseMove(service_id) {
     //
-    var last_x
-    var last_y
+    tvlgnetcast_touch_flag[service_id] = true;
+    //
+    var last_x = 0;
+    var last_y = 0;
+    var current_x = 0;
+    var current_y = 0;
+    var deltaX = 0;
+    var deltaY = 0;
     //
     try {
         //
-        while(MouseEvent.which==1) {
+        while (tvlgnetcast_touch_flag[service_id]) {
             //
-            trackpad_id = "trackpad-" + service_id;
-            var trackpad_obj = document.getElementById(trackpad_id);
+            var trackpad_id = "trackpad-" + service_id;
+            // var trackpad_obj = document.getElementById(trackpad_id);
             //
-            current_x = instanceOfMouseEvent.clientX;
-            current_y = instanceOfMouseEvent.clientY;
+            var offset = $("#" + trackpad_id).offset();
+            $(document).mousemove(function(e){
+                current_x = e.pageX - offset.left;
+                current_y = e.pageY - offset.top;
+            });
             //
             deltaX = last_x - current_x;
             deltaY = last_y - current_y;
             //
-            sendCommand(service_id, {command: 'touchMove', touchMoveX: touchX, touchMoveY: touchY});
+            sendCommand(service_id, {command: 'touchMove', touchMoveX: deltaX, touchMoveY: deltaY});
             //
             last_x = current_x;
             last_y = current_y;
@@ -34,4 +44,9 @@ function tvlgnetcast_touch_mouseMove(service_id) {
     catch(err) {
         alert("An error has been encountered, please try again.\n\n" + err)
     }
+}
+
+
+function tvlgnetcast_touch_mouseUp(service_id) {
+    tvlgnetcast_touch_flag[service_id] = false;
 }
