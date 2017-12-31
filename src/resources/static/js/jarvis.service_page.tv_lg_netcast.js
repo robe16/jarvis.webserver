a681392e909b7e986565a43550ba3e5447a109b6
Rob <robe16@hotmail.co.uk>
2017-12-31 12:25:26 +0000

    Rename JS function to have service type at start of name not end (convention for consistancy on future service JS functions).

--- src/resources/static/js/jarvis.service_page.tv_lg_netcast.js
+++ src/resources/static/js/jarvis.service_page.tv_lg_netcast.js
@@ -1,4 +1,5 @@
-function updateScreenshot_tvlgnetcast(service_id) {
+function tvlgnetcast_updateScreenshot(service_id) {
     newSrc = "/service/image/" + service_id + "/screenshot?time=" + new Date().getTime()
     document.getElementById(service_id + "_screenshot").src = newSrc;
 }
+
