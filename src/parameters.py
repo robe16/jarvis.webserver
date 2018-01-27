8ae980f41d092255745172d65e19d21c9ac2e80d
Rob <robe16@hotmail.co.uk>
2018-01-27 11:52:07 +0000

    Change in broadcast frequency to 60 seconds from 5 seconds to reduce unnecessary network traffic. Discovery paramters adjusted to 2/5 minutes to accommodate increased broadcast duration

--- src/parameters.py
+++ src/parameters.py
@@ -1,6 +1,10 @@
 # seconds
-discovery_cleanup_expiry = 30
-discovery_cleanup_sleep = 30
-discovery_service_mia = 120
+
+# Cleanup variables should be the same, however flexibility provided to have differing values in future
+# cleanup = offline/onlin
+discovery_cleanup_expiry = 120  # 2mins
+discovery_cleanup_sleep = 120  # 2mins
+# mia = missing for x minutes
+discovery_service_mia = 300  # 5mins
 
 bottle_resource_cache_life = 604800
